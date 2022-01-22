3b69dca0d6dc6243876fd75a0988abad7441484f
Josep Conejero <bermeu@gmail.com>
2022-01-22 18:14:04 +0100

    prueba para hacer commits

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    rules: {
+      "no-plusplus": "off",
+      "no-restricted-syntax": "off",
+      "no-debugger": "off",
+      "no-console": "off",
+    },
+  },
+};
