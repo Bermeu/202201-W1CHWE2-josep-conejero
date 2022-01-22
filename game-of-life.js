010ac429c2d8b847ddcdd749e9e0ec2faf080e97
Josep Conejero <bermeu@gmail.com>
2022-01-22 23:09:35 +0100

    se añade estructura básica html, css

--- game-of-life.js
+++ game-of-life.js
@@ -10,14 +10,39 @@ class Tablero {
     this.columnas = 10;
   }
 
-  generarRejillaVacia() {
+  mostrarRejilla() {
+    let cadena = "";
+    for (let fila = 0; fila < this.filas; fila++) {
+      for (let columna = 0; columna < this.columnas; columna++) {
+        cadena += this.rejilla[fila][columna];
+      }
+      console.log(fila, cadena);
+      cadena = "";
+    }
+  }
+
+  generarRejillaLogicaVacia() {
     let rejillaAuxiliar = [];
     for (let fila = 0; fila < this.filas; fila++) {
       for (let columna = 0; columna < this.columnas; columna++) {
         rejillaAuxiliar.push(0);
       }
-      this.panel.push(rejillaAuxiliar);
+      this.rejilla.push(rejillaAuxiliar);
       rejillaAuxiliar = [];
     }
   }
+
+  vaciarRejillaLogica() {
+    this.rejilla = [];
+    this.generarRejillaLogicaVacia();
+  }
+
+  cumpleRegla1() {
+    return true;
+  }
 }
+
+const tabl1 = new Tablero();
+
+tabl1.generarRejillaLogicaVacia();
+tabl1.mostrarRejilla();
